4b6f7b99d2976c1d05f88e48415ab919dde62bde
Nathan Beebe <nbeebe8@gmail.com>
2020-08-16 15:15:39 -0400

    describe and scatter plot

--- main.py
+++ main.py
@@ -2,25 +2,57 @@ import os
 #import data science libraries
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-whitegrid')
 
 #set working directory to CSV to access data
 path = os.chdir('./CSV')
 csv_files_dir = os.listdir(path)
 
+def descriptive_statistics():
+
+    salary_desc = salary_df[['early_career_pay','mid_career_pay']].describe()
+    tuition_desc = tuition_df[['in_state_tuition','out_of_state_tuition']].describe()
+    
+    print('Salary description:\n')
+    print(salary_desc)
+    print('\nTuition description:\n')
+    print(tuition_desc)
+
+     
+
+def scatter_pay_v_STEM():
+    
+    fig = plt.figure()
+    ax = plt.axes()
+
+    ax.plot(salary_df['early_career_pay'],salary_df['stem_percent'],'o')
+
+    plt.title('Pay Versus STEM %')
+    plt.xlabel('Pay')
+    plt.ylabel('% STEM')
+    plt.show()
+    
+
 #define main where functions are run
 if __name__ == "__main__":
 
-    #initialize dataframe
-    df = pd.DataFrame()
-
-    #read files into dataframe
-    for files in csv_files_dir:
-        data = pd.read_csv(files)
-        df = df.append(data)
+    #define dataframes
+    salary_df = pd.read_csv(csv_files_dir[0])
+    tuition_df = pd.read_csv(csv_files_dir[1])
     
-    #print description to terminal
-    print(df.describe())
+    #print dataframe summary
+    print(salary_df)
+    print(tuition_df)
 
-
+    #random sample of ten rows
+    salary_df = salary_df.sample(n=10)
+    tuition_df = tuition_df.sample(n=10)
     
+    #print random sample
+    print(salary_df)
+    print(tuition_df)
+
+    descriptive_statistics()
+
+    scatter_pay_v_STEM()
